f878377570b7993715b73b83349cd6dbb70da751
anas577 <anas.20fathani@gmail.com>
2024-04-10 12:36:13 +0000

    Initialized spring boot project

---
+++ src/main/java/com/example/SubsManagerBackend/SubsManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.SubsManagerBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubsManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubsManagerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SubsManagerBackend/SubsManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SubsManagerBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubsManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
